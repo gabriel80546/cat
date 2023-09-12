8014badc5ea443392b42e5ad7a0618f34f85ded5
Gabriel Passos <gabriel80546@gmail.com>
2023-09-12 04:24:26 -0300

    ./push.sh

--- cat.c
+++ cat.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Ola mundo\n");
+int main(int argc, char *argv[]) {
+    FILE *fptr;
+    int j;
+
+    j = -1;
+    for (int i = 0; i < argc; i++) {
+        if(i > 1) {
+            // printf("argv[%d] = %s\n", i, argv[i]);
+            j++;
+        } else {
+            if(j >= 0) {
+                // Open a file in read mode
+                fptr = fopen(argv[i], "r");
+
+                // Store the content of the file
+                char myString[10000];
+
+                // Read the content and store it inside myString
+                fgets(myString, 10000, fptr);
+
+                // Print the file content
+                printf("%s", myString);
+                // Close the file
+                fclose(fptr);
+            } else {
+                // Open a file in read mode
+                fptr = fopen(argv[i], "r");
+
+                // Store the content of the file
+                char myString[10000];
+
+                // Read the content and store it inside myString
+                fgets(myString, 100, fptr);
+                fclose(fptr);
+            }
+            // j++;
+        }
+    }
     return 0;
 }
